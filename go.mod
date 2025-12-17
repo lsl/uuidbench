948b85a157932d8dc734c648da5e97e78040d21b
Louis Laugesen <louis.laugesen@gmail.com>
2025-12-18 00:41:36 +1100

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/lsl/uuidbench
+
+go 1.25.5
+
+require (
+	github.com/coolaj86/uuidv7 v1.0.2
+	github.com/eucatur/go-toolbox/uuid v1.0.1
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/google/uuid v1.6.0
+	github.com/uuid6/uuid6go-proto v0.2.1
+)
---
+++ uuid_test.go
@@ -0,0 +1,58 @@
+package uuidbench
+
+import (
+	"testing"
+
+	coolaj86 "github.com/coolaj86/uuidv7"
+	toolbox "github.com/eucatur/go-toolbox/uuid"
+	gofrs "github.com/gofrs/uuid"
+	google "github.com/google/uuid"
+	proto "github.com/uuid6/uuid6go-proto"
+)
+
+// https://github.com/gofrs/uuid/blob/master/uuid.go
+// https://github.com/gofrs/uuid/blob/master/generator.go#L412
+func BenchmarkGofrsV7(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = gofrs.NewV7()
+	}
+}
+
+// https://github.com/google/uuid/blob/2d3c2a9cc518326daf99a383f07c4d3c44317e4d/version7.go#L48
+func BenchmarkGoogleV7(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = google.NewV7()
+	}
+}
+
+// https://github.com/eucatur/go-toolbox/blob/6796f092db6ee99c261cd985a509a6719aa65ab6/uuid/uuid_v7.go#L17
+func BenchmarkToolboxV7(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = toolbox.GenerateUUIDv7()
+	}
+}
+
+// https://github.com/uuid6/uuid6go-proto/blob/d8acedd7f415e747b00a65c47e4e242f4f479c52/uuid_v7.go
+func BenchmarkUUID6ProtoV7(b *testing.B) {
+	var test proto.UUIDv7Generator
+	test.SubsecondPrecisionLength = 16
+	test.NodePrecisionLength = 4
+	test.Node = 15
+	test.CounterPrecisionLength = 8
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = test.Next()
+	}
+}
+
+// https://github.com/coolaj86/uuidv7/blob/5eddd0f27f07fc2d4f2f93ff953f44e7cce8bd5b/uuidv7.go#L1
+func BenchmarkCoolAJV7(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = coolaj86.New()
+	}
+}
